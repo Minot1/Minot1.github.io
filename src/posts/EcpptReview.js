38e2df3895735a0be6a36252e83d605b1bb75888
Minot1 <dincer.taner123@gmail.com>
2024-01-24 21:32:49 +0000

    Add eCPPT post

--- src/posts/EcpptReview.js
+++ src/posts/EcpptReview.js
@@ -8,9 +8,9 @@ function EcpptReview() {
     <BasePage>
       <div>
         <Helmet>
-          <meta property="og:title" content="Minot" />
+          <title>Minot - eCPPT Review</title>
+          <meta property="og:title" content="Minot - eCPPT Review" />
           <meta property="og:description" content="eCPPT Review" />
-          <meta property="og:image" content="/eCPPTv2.png" />
         </Helmet>
         <CustomMarkdown path="/eCPPT_review.md"></CustomMarkdown>
       </div>
