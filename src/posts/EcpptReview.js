5a2ffd302ea943064ca09ce76f8d2c640b5afe77
Minot1 <dincer.taner123@gmail.com>
2024-01-24 19:21:10 +0000

    Add eCPPT post

--- src/posts/EcpptReview.js
+++ src/posts/EcpptReview.js
@@ -1,11 +1,15 @@
 import React from "react";
 import BasePage from "../components/BasePage";
 import CustomMarkdown from "../components/CustomMarkdown";
+import { Helmet } from "react-helmet";
 
 function EcpptReview() {
   return (
     <BasePage>
       <div>
+        <Helmet>
+          <meta property="og:image" content="/eCPPTv2.png" />
+        </Helmet>
         <CustomMarkdown path="/eCPPT_review.md"></CustomMarkdown>
       </div>
     </BasePage>
